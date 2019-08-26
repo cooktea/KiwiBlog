da1452d9feef5d2dbb8e54a392028eea9b788749
cooktea <623285624@qq.com>
2019-08-27 00:24:17 +0800

    add articles page

--- js/mymock.js
+++ js/mymock.js
@@ -1,23 +1,46 @@
 Mock.setup({
-    timeout:100-400
+    timeout:"500-1000"
+})
+
+Mock.mock('http://text.com/getArticleTypes',{
+    "types|4-10":[{
+        "name":"@word()"
+    }]
 })
 
 Mock.mock('http://test.com/getNews', {
     "news|2":[{
         "date":"@date(yyyy-MM-dd)",
-        "content":"@sentence"
+        "content":"@sentence(40)"
     }]
 });
-Mock.mock('http://test.com/getArticles',{
-    "articles|12":[{
-        "date":"@date(yyyy-MM-dd)",
-        "title":"@title",
-        "content":"@sentence",
-        "image":"@image",
-        "src":Mock.Random.url("http"),
-        "tags|1-3":[{
-            "type":"@word",
-            "src":"@url(http)"
+// Mock.mock(/test.com\/getArticles/,{
+//     "articles|12":[{
+//         "date":"@date(yyyy-MM-dd)",
+//         "title":"@title",
+//         "content":"@sentence",
+//         "image":"@image(200x400)",
+//         "src":Mock.Random.url("http"),
+//         "tags|1-3":[{
+//             "type":"@word",
+//             "src":"@url(http)"
+//         }]
+//     }]
+// })
+
+Mock.mock(/test.com\/getArticles/,function(option){
+    console.log(option)
+    return Mock.mock({
+        "articles|12":[{
+            "date":"@date(yyyy-MM-dd)",
+            "title":"@title",
+            "content":"@sentence",
+            "image":"@image(200x400)",
+            "src":Mock.Random.url("http"),
+            "tags|1-3":[{
+                "type":"@word",
+                "src":"@url(http)"
+            }]
         }]
-    }]
+    })
 })
