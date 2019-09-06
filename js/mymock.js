aaaba19d53fdbfcc361c8ff0aad38c961f86c1f9
cooktea <623285624@qq.com>
2019-09-06 17:41:05 +0800

    add statistic infomation block

--- js/mymock.js
+++ js/mymock.js
@@ -1,20 +1,27 @@
 Mock.setup({
-    timeout:"500-1000"
+    timeout:"1000-2000"
 })
-
+// 获取文章的分类
 Mock.mock('http://text.com/getArticleTypes',{
     "types|4-10":[{
         "name":"@word()"
     }]
 })
-
+// 获取网站的统计信息
+Mock.mock('http://test.com/getStatisticsInfo',{
+        "articleCount|0-200":100,
+        "dayVisitCount|0-200":100,
+        "monthVisitCount|30-6000":100
+    
+})
+// 获取最近动态
 Mock.mock('http://test.com/getNews', {
     "news|2":[{
         "date":"@date(yyyy-MM-dd)",
         "content":"@sentence(40)"
     }]
 });
-
+// 获取文章
 Mock.mock(/test.com\/getArticles/,function(option){
     console.log(option)
     return Mock.mock({
