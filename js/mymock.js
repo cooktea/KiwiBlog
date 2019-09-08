0afefa7ca68eb98d623c909165cca1eb236f5d2d
cooktea <623285624@qq.com>
2019-09-08 22:47:16 +0800

    login page done

--- js/mymock.js
+++ js/mymock.js
@@ -1,5 +1,10 @@
 Mock.setup({
-    timeout:"1000-2000"
+    timeout:"100-200"
+})
+// 模拟登陆，获取token
+Mock.mock('http://test.com/login',{
+    "result":"success",
+    "token":"@word(10)"
 })
 // 获取统计信息
 Mock.mock('http://test.com/getStatistic',{
@@ -7,16 +12,15 @@ Mock.mock('http://test.com/getStatistic',{
         "date":"@date(MM-dd)",
         "count|0-1000":100
     }],
-    "monthInfo|7":[{
+    "monthInfo|12":[{
         "date":"@date(yyyy-MM)",
         "count|0-30000":100
     }],
-    "yearInfo|7":[{
+    "yearInfo|1-6":[{
         "date":"@date(yyyy)",
         "count|0-365000":100
     }]
 })
-
 // 获取文章的分类
 Mock.mock('http://text.com/getArticleTypes',{
     "types|4-10":[{
@@ -41,7 +45,7 @@ Mock.mock('http://test.com/getNews', {
 Mock.mock(/test.com\/getArticles/,function(option){
     console.log(option)
     return Mock.mock({
-        "articles|9":[{
+        "articles|0-12":[{
             "date":"@date(yyyy-MM-dd)",
             "title":"@title",
             "content":"@sentence",
